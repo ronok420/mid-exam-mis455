ba955c3901e791a06b3c285c4301c38279e28373
ronok420 <shahariarronok41@gmail.com>
2023-11-11 23:25:00 +0600

    contact page added

---
+++ script.js
@@ -0,0 +1,8 @@
+function contactForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var suggestion = document.getElementById("suggestion").value;
+
+    var displayInputDiv = document.getElementById("displayInput");
+    displayInputDiv.innerHTML = `<h2>Contact Information</h2><p><strong>Name:</strong>  ${name}  </p><p><strong>Email:</strong>   ${email}  </p><p><strong>Suggestion:</strong>   ${suggestion}  "</p>`;
+}
