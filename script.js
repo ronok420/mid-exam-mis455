07203f3c62c09b34a42a62ecdc62ed269d4227b8
ronok420 <shahariarronok41@gmail.com>
2023-11-11 23:27:29 +0600

    last commit

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function contactForm() {
     var suggestion = document.getElementById("suggestion").value;
 
     var displayInputDiv = document.getElementById("displayInput");
-    displayInputDiv.innerHTML = `<h2>Contact Information</h2><p><strong>Name:</strong>  ${name}  </p><p><strong>Email:</strong>   ${email}  </p><p><strong>Suggestion:</strong>   ${suggestion}  "</p>`;
+    displayInputDiv.innerHTML = `<h2>Contact Information</h2><p><strong>Name:</strong>  ${name}  </p><p><strong>Email:</strong>   ${email}  </p><p><strong>Suggestion:</strong>   ${suggestion}  </p>`;
 }
